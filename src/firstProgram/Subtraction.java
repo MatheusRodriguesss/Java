d1c23baa48f7b10b127c00ee6e6b2c5929d3acfd
Matheus Rodrigues <contato.matheusrodrigues3008@gmail.com>
2024-02-20 13:43:21 -0300

    updating files of the first exercises

--- src/firstProgram/Subtraction.java
+++ src/firstProgram/Subtraction.java
@@ -1,8 +1,7 @@
 package firstProgram;
 public class Subtraction {
     public static void main(String[] args) {
-        System.out.print("This is the result of a subtraction: ");
-        System.out.println(10-5);
-
+        int less = 10 - 5;
+        System.out.print("This is the result of a subtraction: " + less);
     }
 }
---
+++ src/screenMatch/Main.java
@@ -0,0 +1,11 @@
+package screenMatch;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Screen Match!");
+        System.out.println("Step up 3");
+
+        int year = 2010 - 2;
+        System.out.println(year);
+
+        }
+    }
